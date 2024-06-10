7d4f6576283ba3d223b8216f7fcee685e5863687
Nathan Larson <xenloops@protonmail.com>
2024-06-10 16:50:03 -0500

    Create listProjects.py

---
+++ listProjects.py
@@ -0,0 +1,12 @@
+import json
+import urllib3
+
+# Set SonarQube URL here or get it safely from User
+url = ""
+apiSuffix = "/api/components/search_projects?ps=500&p=1"
+
+rawData = urllib3.urlopen(url & apiSuffix)
+parseData = rawData.read()
+dictData = json.loads(parseData)
+for i in range(100):
+    print (dictData['components'][i]['name'])
